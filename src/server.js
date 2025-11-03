bd8895afafe5c2b327b8343bf9752cd71aa906cf
rishabhjain2010 <jainrishabh.201@gmail.com>
2025-11-04 03:24:55 +0530

    server.js created

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ShopNow+ Backend Server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
